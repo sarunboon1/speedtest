cae25dafebf14a5f68f4a5cdd26e4e6e61e9a8cb
Sarun Boonkoonsak <sarunboon@gmail.com>
2020-11-27 12:35:20 +0700

    Remove confident info
    Remove user/pass change to xxx and remove mailing list

--- 10_speedtest.sh
+++ 10_speedtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/cgn_test/speedtestcsv
-user="admin"
-pass="comnet@123"
+user="xxx"
+pass="xxx"
 now=`date +"%Y-%m-%d"`
 timestamp=`date +"%r"`
 profilearray=(BRK_707_public CMI_701_1000M HYI_711_public HYI_PE1_711_1000M KKN_705_1000M KKN_705_public NSW_703_1000M NSW_703_public SDI_712_1000M SDL_712_public)
--- 999_mail.sh
+++ 999_mail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/cgn_test/speedtestcsv
-user="admin"
-pass="comnet@123"
+user="xxx"
+pass="xxx"
 now=`date +"%Y-%m-%d"`
 profilearray=(BRK_707_1000M BRK_707_public CMI_701_1000M HYI_711_public HYI_PE1_711_1000M KKN_705_1000M KKN_705_public NSW_703_1000M NSW_703_public SDI_712_1000M SDL_712_public)
 echo $profilearray
@@ -13,4 +13,4 @@ do
 	zip -r ${now}.zip $current_file
 done
 ##################################################################################
-echo "speedtestcsv private" | mail -s "CAT BB Speedtest Private Account ${now} $timestamp" sarun@comnet.in.th vachara@comnet.in.th, amnad@comnet.in.th -A ${now}.zip
+echo "speedtestcsv private" | mail -s "CAT BB Speedtest Private Account ${now} $timestamp" xxx@xxx -A ${now}.zip
